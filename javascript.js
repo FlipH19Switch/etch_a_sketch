642b7c8f6115b6bf308682f04d15fe3a286386a5
Zach <107371302+zpsell01@users.noreply.github.com>
2023-08-09 11:32:08 -0400

    Get user input for grid size

--- javascript.js
+++ javascript.js
@@ -4,16 +4,34 @@ Containers must have a 16 x 16 grid layout via Flexbox
 Containers must share same styling and be interactive via hover
 */
 
-const container = document.querySelector('#container');
-const gridSquareNumber = 16 * 16
 
-// create the grid
-for (let i = 0; i < gridSquareNumber; i++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    gridSquare.id = "grid-square";
-    container.appendChild(gridSquare);
+
+const container = document.querySelector('#container');
+
+btn.addEventListener('click', () => {
+    createGrid()
+})
+
+function createGrid() {
+    let gridSideLength = +prompt("How many squares per side would you like?", );
+    
+    while (gridSideLength > 100) {
+        gridSideLength = +prompt("That is too large. Please enter a maximum of 100 squares.", )
+    }
+    
+    // creates grid
+    for (let i = 0; i < Math.pow(gridSideLength,2); i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        gridSquare.id = "grid-square";
+        container.appendChild(gridSquare);
+    }
+
+    // adds outline to parent div only after grid is created
+    container.classList.add('container');
 }
+
+
 
 // change background of hovered squares
 const hoveredSquares = document.querySelectorAll('#grid-square');
